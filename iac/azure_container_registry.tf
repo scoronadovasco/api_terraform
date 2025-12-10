f5245ffdf666cab5e0537cabbaeea59119a999bc
scoronadovasco <scoronadovasco@gmail.com>
2025-12-09 22:03:16 -0500

    add Azure Container Registry resource configuration

---
+++ iac/azure_container_registry.tf
@@ -0,0 +1,11 @@
+
+resource "azurerm_container_registry" "acr_api_net" {
+  name                = "containerapinet"
+  resource_group_name = azurerm_resource_group.RG_API_NET.name
+  location            = azurerm_resource_group.RG_API_NET.location
+  sku                 = "Standard"
+  admin_enabled       = false
+  tags = merge(var.tags, {
+    "descriptipn" = "container for docker images"
+  })
+}
