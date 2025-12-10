d459044fd66f019f8e54d2baa61a4316d1390b7b
scoronadovasco <scoronadovasco@gmail.com>
2025-12-09 19:26:30 -0500

    initialize Terraform configuration files and add Azure provider settings

---
+++ iac/vars.tf
@@ -0,0 +1,10 @@
+variable "env_id" {
+  type        = string
+  description = "enviroment for develoment"
+  default     = "dev"
+}
+variable "subscription_id" {
+  type = string
+  description = "azure description id"
+  default = "34820a7f-49a4-448c-adab-ee7b09d9403b"
+}
