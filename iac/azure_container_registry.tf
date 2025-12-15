c88a9be123058b3b7da41093a19d85fa368cad78
scoronadovasco <scoronadovasco@gmail.com>
2025-12-15 18:35:01 -0500

    fix: enable admin access for Azure Container Registry and update SQL database firewall rules

--- iac/azure_container_registry.tf
+++ iac/azure_container_registry.tf
@@ -4,7 +4,7 @@ resource "azurerm_container_registry" "acr_api_net" {
   resource_group_name = azurerm_resource_group.RG_API_NET.name
   location            = azurerm_resource_group.RG_API_NET.location
   sku                 = "Standard"
-  admin_enabled       = false
+  admin_enabled       = true
   tags = merge(var.tags, {
     "descriptipn" = "container for docker images"
   })
--- iac/azure_sqldatabase.tf
+++ iac/azure_sqldatabase.tf
@@ -1,10 +1,14 @@
 resource "azurerm_mssql_server" "api-net-sqlserver" {
-  name                         = "api-net-sqlserver-1205"
-  resource_group_name          = azurerm_resource_group.RG_API_NET.name
-  location                     = var.location
-  version                      = "12.0"
-  administrator_login          = var.userdb
-  administrator_login_password = var.userdbpass
+  name                          = "api-net-sqlserver-1205"
+  resource_group_name           = azurerm_resource_group.RG_API_NET.name
+  location                      = var.location
+  version                       = "12.0"
+  administrator_login           = var.userdb
+  administrator_login_password  = var.userdbpass
+  public_network_access_enabled = true
+  
+  # Configuración mínima de seguridad TLS
+  minimum_tls_version = "1.2"
 }
 
 resource "azurerm_mssql_database" "sqldatabase-apinet" {
@@ -25,9 +29,18 @@ resource "azurerm_mssql_database" "sqldatabase-apinet" {
   }
 }
 
-resource "azurerm_mssql_firewall_rule" "sql_rule" {
-  end_ip_address   = "0.0.0.0"
-  start_ip_address = "0.0.0.0"
+# Regla para permitir servicios de Azure (Container Apps, Functions, etc.)
+resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+  name             = "AllowAzureServices"
   server_id        = azurerm_mssql_server.api-net-sqlserver.id
-  name             = "azsql-firewall-rule"
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+# Regla específica para Container App outbound IP
+resource "azurerm_mssql_firewall_rule" "allow_container_app" {
+  name             = "AllowContainerApp"
+  server_id        = azurerm_mssql_server.api-net-sqlserver.id
+  start_ip_address = "52.226.99.237"
+  end_ip_address   = "52.226.99.237"
 }
