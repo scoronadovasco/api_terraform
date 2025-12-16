d988df4590072da635bb17265d2f4df6bebd51ff
scoronadovasco <scoronadovasco@gmail.com>
2025-12-15 21:14:00 -0500

    feat: add GitHub Actions workflow for continuous integration and configure Azure backend for Terraform

--- iac/providers.tf
+++ iac/providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.55.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "rg_api_net"
+    storage_account_name = "storageaccountapinet"
+    container_name = "terraform"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
